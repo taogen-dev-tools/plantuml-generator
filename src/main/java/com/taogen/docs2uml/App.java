74d9137c2be4950ff5296b40904a7e6fdcc338b9
taogen <taogenjia@gmail.com>
2020-03-16 16:18:50 +0800

    chore: Build project

---
+++ src/main/java/com/taogen/docs2uml/App.java
@@ -0,0 +1,13 @@
+package com.taogen.docs2uml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
