1e7e7dbae2caed3711cecf8e4a8ab5fda56c7641
taogen <taogenjia@gmail.com>
2020-03-19 19:46:35 +0800

    refactor: Update entities

--- src/main/java/com/taogen/docs2uml/entity/MyCommand.java
+++ src/main/java/com/taogen/docs2uml/entity/MyCommand.java
@@ -1,11 +1,28 @@
 package com.taogen.docs2uml.entity;
 
+import lombok.Data;
+
 /**
  * @author Taogen
  */
+@Data
 public class MyCommand {
     private String url;
     // TODO: update doc, pacakge -> pacakgeName
     private String packageName;
     private Boolean subPackage;
+
+    public MyCommand() {
+    }
+
+    public MyCommand(String url, String packageName) {
+        this.url = url;
+        this.packageName = packageName;
+    }
+
+    public MyCommand(String url, String packageName, Boolean subPackage) {
+        this.url = url;
+        this.packageName = packageName;
+        this.subPackage = subPackage;
+    }
 }
--- src/main/java/com/taogen/docs2uml/entity/MyEntity.java
+++ src/main/java/com/taogen/docs2uml/entity/MyEntity.java
@@ -1,20 +1,22 @@
 package com.taogen.docs2uml.entity;
 
 import com.taogen.docs2uml.constant.EntityType;
+import lombok.Data;
 
 import java.util.List;
 
 /**
  * @author Taogen
  */
+@Data
 public class MyEntity {
     private EntityType type;
     // TODO: update doc package -> packageName
     private String packageName;
     private String className;
+    private String url;
     private MyEntity parentClass;
     private List<MyEntity> parentInterfaces;
     private List<MyField> fields;
     private List<MyMethod> methods;
-
 }
