2e4a76ece3df0071e072243fd9f703ebb3e0be57
taogen <taogenjia@gmail.com>
2020-03-19 19:45:01 +0800

    refactor: Update CommandHandlerTest

--- src/test/java/com/taogen/docs2uml/command/CommandHandlerTest.java
+++ src/test/java/com/taogen/docs2uml/command/CommandHandlerTest.java
@@ -1,7 +1,6 @@
 package com.taogen.docs2uml.command;
 
 import com.taogen.docs2uml.constant.CommandError;
-import com.taogen.docs2uml.entity.ErrorMessage;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,7 +12,24 @@ public class CommandHandlerTest {
     @Test
     public void check() {
         commandHandler.setArguments(null);
-        ErrorMessage errorMessage = commandHandler.check();
-        assertEquals(CommandError.ERROR_CODE_MISS_PARAM, errorMessage.getErrorCode());
+        assertEquals(CommandError.ERROR_CODE_MISS_PARAM, commandHandler.check().getErrorCode());
+
+        commandHandler.setArguments(new String[]{"-u", "http://test.com"});
+        assertEquals(CommandError.ERROR_CODE_MISS_PARAM, commandHandler.check().getErrorCode());
+
+        commandHandler.setArguments(new String[]{"-u", "h://test.com", "-p", "com.test"});
+        assertEquals(CommandError.ERROR_CODE_PARAM_VALUE_FORMAT_ERROR, commandHandler.check().getErrorCode());
+
+        commandHandler.setArguments(new String[]{"-u", "https://test.com", "-s", "t", "-p", "com.test"});
+        assertEquals(CommandError.ERROR_CODE_PARAM_VALUE_FORMAT_ERROR, commandHandler.check().getErrorCode());
+
+        commandHandler.setArguments(new String[]{"-u", "https://test.com", "-p", "com.test"});
+        assertEquals(CommandError.SUCCESS_CODE, commandHandler.check().getErrorCode());
+
+        commandHandler.setArguments(new String[]{"-p", "com.test", "-u", "https://test.com"});
+        assertEquals(CommandError.SUCCESS_CODE, commandHandler.check().getErrorCode());
+
+        commandHandler.setArguments(new String[]{"-u", "https://test.com", "-p", "com.test", "-s", "true"});
+        assertEquals(CommandError.SUCCESS_CODE, commandHandler.check().getErrorCode());
     }
 }
