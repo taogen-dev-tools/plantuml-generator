172c2fdc9cc766ecad149af0283859d5b70dcfec
taogen <taogenjia@gmail.com>
2020-03-20 09:59:47 +0800

    test: Add test of ClassDetailsCrawler

---
+++ src/test/java/com/taogen/docs2uml/crawler/impl/ClassDetailsCrawlerTest.java
@@ -0,0 +1,23 @@
+package com.taogen.docs2uml.crawler.impl;
+
+import com.taogen.docs2uml.entity.MyCommand;
+import com.taogen.docs2uml.entity.MyEntity;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ClassDetailsCrawlerTest extends CrawlerTest{
+
+    private ClassDetailsCrawler classDetailsCrawler = new ClassDetailsCrawler();
+
+    @Test
+    public void crawl() {
+        String url = "https://docs.oracle.com/javase/8/docs/api/java/io/BufferedInputStream.html";
+        classDetailsCrawler.setMyCommand(new MyCommand(url));
+        List<MyEntity> myEntities = classDetailsCrawler.crawl();
+        assertNotNull(myEntities);
+        assertEquals(1, myEntities.size());
+    }
+}
