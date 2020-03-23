f8b3d8a4fac8ca2fc1d38075bce3fe62c377862f
taogen <taogenjia@gmail.com>
2020-03-23 16:48:15 +0800

    test: fix test NullPointerException

--- src/test/java/com/taogen/docs2uml/crawler/impl/ClassDetailsCrawlerTest.java
+++ src/test/java/com/taogen/docs2uml/crawler/impl/ClassDetailsCrawlerTest.java
@@ -15,7 +15,7 @@ public class ClassDetailsCrawlerTest extends CrawlerTest{
     @Test
     public void crawl() {
         String url = "https://docs.oracle.com/javase/8/docs/api/java/io/BufferedInputStream.html";
-        MyCommand myCommand = new MyCommand(url, "java.io");
+        MyCommand myCommand = new MyCommand(url, "java.io", "java.io");
 //        classDetailsCrawler.setMyCommand(myCommand);
         List<MyEntity> myEntities = classDetailsCrawler.crawl(myCommand);
         assertNotNull(myEntities);
