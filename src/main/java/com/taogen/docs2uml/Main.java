7be3109c4dc1307a81fae5a663d67f8c24b493a3
taogen <taogenjia@gmail.com>
2020-03-23 18:58:49 +0800

    refactor: Finished main

--- src/main/java/com/taogen/docs2uml/Main.java
+++ src/main/java/com/taogen/docs2uml/Main.java
@@ -1,7 +1,10 @@
 package com.taogen.docs2uml;
 
+import com.taogen.docs2uml.command.CommandHandler;
+import com.taogen.docs2uml.constant.CommandError;
 import com.taogen.docs2uml.entity.MyCommand;
 import com.taogen.docs2uml.entity.MyEntity;
+import com.taogen.docs2uml.exception.KnownException;
 import com.taogen.docs2uml.generator.Generator;
 import com.taogen.docs2uml.generator.impl.ClassDiagramGenerator;
 import com.taogen.docs2uml.task.TaskController;
@@ -17,12 +20,26 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        long beginTime = System.currentTimeMillis();
-        TaskController taskController = new TaskController(new MyCommand("https://docs.oracle.com/javase/8/docs/api/index.html", "java.io"));
-        List<MyEntity> myEntityList = taskController.execute();
-        Generator generator = new ClassDiagramGenerator();
-        generator.generate(myEntityList);
-        logger.info("Elapsed time is {}" + (System.currentTimeMillis() - beginTime));
+        CommandHandler commandHandler = new CommandHandler(args);
+        if (! CommandError.SUCCESS_CODE.equals(commandHandler.check().getErrorCode())){
+            CommandHandler.showCommandUsage();
+            return;
+        }
+        MyCommand myCommand = commandHandler.getMyCommand();
+        try {
+            long beginTime = System.currentTimeMillis();
+            TaskController taskController = new TaskController(myCommand);
+            List<MyEntity> myEntityList = taskController.execute();
+            Generator generator = new ClassDiagramGenerator();
+            generator.generate(myEntityList);
+            logger.info("Elapsed time: {}ms", (System.currentTimeMillis() - beginTime));
+        }catch (Exception e){
+            if (e instanceof KnownException){
+                logger.error("{}: {}", e.getClass().getName(), e.getMessage(), e);
+            }else{
+                logger.error("System internal error!", e);
+            }
+        }
     }
 
 }
